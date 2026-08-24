2d6f76f45dc1c3dea1c54a62584eb6ca52a734d3
ryan-griffin <85765087+ryan-griffin@users.noreply.github.com>
2026-08-24 16:36:27 -0400

    Add CSS file serving

--- server.js
+++ server.js
@@ -5,10 +5,13 @@ const http = require('http'),
 const server = http.createServer( function( request,response ) {
   switch( request.url ) {
     case '/':
-      sendFile( response, 'index.html' )
+      sendFile( response, 'index.html', 'text/html' )
       break
     case '/index.html':
-      sendFile( response, 'index.html' )
+      sendFile( response, 'index.html', 'text/html' )
+      break
+    case '/style.css':
+      sendFile( response, 'style.css', 'text/css' )
       break
     default:
       response.end( '404 Error: File Not Found' )
@@ -17,7 +20,8 @@ const server = http.createServer( function( request,response ) {
 
 server.listen( process.env.PORT || port )
 
-const sendFile = function( response, filename ) {
+const sendFile = function( response, filename, contentType ) {
+   response.writeHead( 200, { 'Content-Type': contentType } )
    fs.readFile( filename, function( err, content ) {
      response.end( content, 'utf-8' )
    })
